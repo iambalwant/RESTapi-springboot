48c258d52313ac170d1d26c25bf2808e173a15c2
iambalwant <balwantverma55555@gmail.com>
2025-10-23 12:00:37 +0530

    RESTapi intergration with lombok

--- src/main/java/com/edigest/myfirstproject/entity/journalEntry.java
+++ src/main/java/com/edigest/myfirstproject/entity/journalEntry.java
@@ -1,5 +1,8 @@
 package com.edigest.myfirstproject.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +12,9 @@ import java.util.Date;
 
 //@Document tells Spring Boot that this class represents a MongoDB collection.
 @Document(collection = "journal_Entries")
+//@Getter
+//@Setter //by lombok to reduce the boilPlate and install plugin
+@Data //Equivalent to @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
 public class journalEntry {
     @Id //to map as primary (unique) key
     private ObjectId id;
@@ -17,36 +23,37 @@ public class journalEntry {
     private LocalDateTime date;
 
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//----------use to reduce the boilerplate with lombok project------------
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
+//
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 }
