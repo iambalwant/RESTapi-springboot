0acc7fa2b81978936bdf2e0ad60ca57ff99644b2
iambalwant <balwantverma55555@gmail.com>
2025-10-27 12:42:13 +0530

    Unit test @Mock @InjectMocks

---
+++ src/test/java/com/edigest/myfirstproject/service/UserDetailsServiceImpTest.java
@@ -0,0 +1,42 @@
+package com.edigest.myfirstproject.service;
+
+import com.edigest.myfirstproject.entity.userEntry;
+import com.edigest.myfirstproject.repository.userEntryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+public class UserDetailsServiceImpTest {
+
+    @InjectMocks
+    private userEntryRepository userEntryRepository;
+
+    @Mock
+    private UserDetailServicimpl userDetailServicimpl;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsername(){
+        when(userEntryRepository.findByusername(ArgumentMatchers.anyString())).thenReturn((userEntry) User.builder().username("ram").password("werwer").roles(String.valueOf(new ArrayList<>())).build());
+        UserDetails user = userDetailServicimpl.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+
+    }
+}
